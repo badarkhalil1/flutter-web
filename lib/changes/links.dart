c9ddc3180f9890f58a4798a37a5f3a79c929bb46
unknown <flutterdeveloper786@gmail.com>
2023-07-02 09:01:19 +0500

    first commit

---
+++ lib/changes/links.dart
@@ -0,0 +1,38 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1DOKE85UpUYHHn66-PBDGb9qM49LU0gc0/view?usp=drive_link';
+
+// Github Link
+const String gitHub = 'https://github.com/badarkhalil1';
+// Github Link
+const String facebook = 'https://www.facebook.com/profile.php?id=100083674726953&mibextid=ZbWKwL';
+// Github Link
+const String twitter = 'https://twitter.com/Badarkhalil12?t=kwvlqGmu7vofZ5vVAPmOQA&s=08';
+// Github Link
+const String youtube =
+    'https://www.youtube.com/channel/UCBDjKBPoK0dZ_V_3vLWsQEw';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/badar-khalil786';
+// Github Link
+const String buymeacoffee = '';
+// Github Link
+const String instagram = 'https://instagram.com/badarkhalil1?igshid=MzNlNGNkZWQ4Mg==';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage =
+    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+const String fiverr = 'https://www.fiverr.com/badarkhalilkhan?up_rollout=true';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~01b79ab51312e79faa';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=+923090313010';
---
+++ lib/changes/strings.dart
@@ -0,0 +1,29 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Badar Khalil";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = " Mobile Application Developer";
+String animationtxt2 = " UI/UX Designer";
+String animationtxt3 = " Web Developer";
+String animationtxt4 = " Flutter Development";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a freelance designer and developer, I've worked in startups and collaborated with talented people to create digital products for both business and consumer use. I offer a wide range of services, including brand design, programming and teaching.";
+//
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use.";
+const String aboutMeHeadline =
+    'I\'m Badar Khalil, a Flutter App developer, Web developer,Desktop Application Developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m a Fresh Graduate in BS-CS from NCBA&E University, Pk. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Currently working as a Freelancer at Fiver.';
